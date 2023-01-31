2a13986d2cfb3c75076907c4cee8f21c9e60a74c
DariaPyvovarovaQAMadness <d.pyvovarova@arammeem.com>
2023-01-31 16:58:17 +0200

    add allure coverage

--- src/main/java/api/utils/ApiServices.java
+++ src/main/java/api/utils/ApiServices.java
@@ -1,16 +1,21 @@
 package api.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured;
+import io.qameta.allure.Allure;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import lombok.SneakyThrows;
 
 import java.util.Map;
 
 import static api.utils.Base.testProp;
+import static org.apache.http.HttpHeaders.DATE;
 
 public class ApiServices {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static Response postRequest(String endpoint, Map<String, Object> headers, Object body, Map<String, String> queryParams, Map<String, String> pathParams) {
         RequestSpecification requestSpecification = RestAssured.given();
@@ -43,6 +48,8 @@ public class ApiServices {
 
         Response resp = requestSpecification.post(testProp.BASE_URL() + endpoint);
 
+        Allure.addAttachment("Response", getAllureResponse(testProp.BASE_URL() + endpoint, resp, body));
+
 
         return resp;
     }
@@ -73,6 +80,8 @@ public class ApiServices {
                     .all();
         }
         Response resp = requestSpecification.get(testProp.BASE_URL() + endpoint);
+
+        Allure.addAttachment("Response", getAllureResponse(testProp.BASE_URL() + endpoint, resp, body));
 
 
         return resp;
@@ -111,6 +120,8 @@ public class ApiServices {
 
         Response resp = requestSpecification.put(testProp.BASE_URL() + endpoint);
 
+        Allure.addAttachment("Response", getAllureResponse(testProp.BASE_URL() + endpoint, resp, body));
+
         return resp;
     }
 
@@ -146,6 +157,23 @@ public class ApiServices {
 
         Response resp = requestSpecification.delete(testProp.BASE_URL() + endpoint);
 
+        Allure.addAttachment("Response", getAllureResponse(testProp.BASE_URL() + endpoint, resp, body));
+
         return resp;
+    }
+
+    @SneakyThrows
+    public static String getAllureResponse(String endpoint, Response resp, Object body) {
+
+        String request_date = resp.getHeader(DATE);
+        String responseStatus = String.valueOf(resp.getStatusCode());
+        String requestBody = body != null ? objectMapper.writeValueAsString(body) : "";
+
+
+        return "\n\nRequest date: " + request_date +
+                "\n\nEndpoint: " + endpoint +
+                "\n\nRequest body: " + requestBody +
+                "\n\nResponse status code: " + responseStatus +
+                "\n\nResponse: " + resp.getBody().asString();
     }
 }
